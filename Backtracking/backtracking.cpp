ba1b250e79dd546496b65c1ba0a3821896eeac7e
William-Bruno <williambrunoclar@gmail.com>
2022-04-19 09:56:52 -0300

    Backtracking

---
+++ Backtracking/backtracking.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+   
+bool verifica_insercao(std::string& line, int index, int value, int proximidade){
+    for(int i = index + 1; i < index + 1 + proximidade; i++){
+        if(i < (int)line.size() && line[i] == value + '0'){
+            return false;
+        }
+    }
+    for(int i = index - proximidade; i < index; i++){
+        if(i >= 0 && line[i] == value + '0'){
+            return false;
+        }
+    }
+    return true;
+}
+
+bool preencher(std::string& line, int index, int limite, int proximidade){
+    if((int)line.size() == index){
+        std::cout << line << " <------\n";
+        return true;
+    }
+    if(line[index]  != '.'){
+        return preencher(line, index + 1, limite, proximidade);
+    }
+    for(int value = 0; value <= limite; value++){
+        if(verifica_insercao(line, index, value, proximidade)){
+            line[index] = value + '0';
+            std::cout << line << "\n";
+            if(preencher(line, index + 1, limite, proximidade)){
+                return true;
+            }
+        }
+    }
+    line[index] = '.';
+    std::cout << line << "\n";
+    return false;
+}
+   
+int main() {
+    std::ifstream arquivo("backtracking.txt");
+    std::string line;
+    int proximidade = 0;
+    arquivo >> line >> proximidade;
+    int limite = 2;
+    preencher(line, 0, limite, proximidade);
+}
